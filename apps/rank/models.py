4662fe4a2029bb7f84c911ac037473722c89d1b1
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-07-20 00:46:06 +0900

    Revert "Merge pull request #1 from Pirogramming-21/develop"
    This reverts commit 6fd20c3bf7dcf204e06fef0b4e15e373e0240faf, reversing
    changes made to e1a03828691f0771911d2b69aee6c1fe8c156a71.

--- apps/rank/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from apps.users.models import User
-
-# Create your models here.
-class Rank(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.IntegerField(default=0)
-
--- apps/rank/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "rank"
-
-urlpatterns = [
-    path("list/", views.ranking, name = "ranking"),
-]
