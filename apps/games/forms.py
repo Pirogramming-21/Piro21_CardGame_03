8d63be32e8878b345b0b07b76fe12bfa4452573f
Son Kwanwoo <sonkw4775@gmail.com>
2024-07-19 17:52:20 +0900

    [feat] 닉네임 사용 오류 해, css 수정

--- apps/games/forms.py
+++ apps/games/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from apps.users.models import User
 from .models import Game
-import random
 
 class GameForm(forms.ModelForm):
-    opponent = forms.ModelChoiceField(queryset=User.objects.all(), label="상대 유저 선택")
+    opponent = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        label="상대 유저 선택"
+    )
+
     player1_card = forms.IntegerField(widget=forms.HiddenInput())
 
     def __init__(self, *args, **kwargs):
@@ -15,6 +18,8 @@ class GameForm(forms.ModelForm):
             self.fields['opponent'].queryset = User.objects.exclude(id=self.request.user.id)
         if random_numbers:
             self.fields['player1_card'].choices = [(num, num) for num in random_numbers]
+
+        self.fields['opponent'].label_from_instance = lambda obj: obj.nickname
 
     class Meta:
         model = Game
